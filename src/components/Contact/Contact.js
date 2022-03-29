47d7ec2d6322cd2f630bb0976047e36bbd3c0528
rakky21 <rakky26@gmail.com>
2022-03-28 23:51:47 -0400

    changed sizing of the labels

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,20 +2,20 @@ import React from "react";
 
 function Contact() {
   return (
-    <section className="container col-6 g-3 my-2 " class="form" style={{ border: "1px solid",  }}>
+    <section className="col-6 my-2 " class="form" style={{ border: "1px solid",  }}>
 
       <div className="my-2">
-        <label className="col-6 my-2">First name
+        <label className="col-4 my-2">First name
           <input className="form-control my-2" placeholder="First name" aria-label="First name"></input></label>
 
-        <label className="col-6 my-2">Last name
+        <label className="col-4 my-2">Last name
         <input className="form-control my-2" placeholder="Last name" aria-label="Last name"></input></label>
   
   <div className="my-2">
-        <label className="col-6 form-label my-2">E-mail
+        <label className="col-4 form-label my-2">E-mail
         <input className="form-control my-2" placeholder="E-mail" aria-label="E-mail"></input></label>
 
-        <label className="col-6 form-label my-2">Telephone
+        <label className="col-4 form-label my-2">Telephone
         <input className="form-control my-2" placeholder="Telephone" aria-label="Telephone"></input></label>
         </div>
       </div>
