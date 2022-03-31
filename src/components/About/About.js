0e79c4cb15b4e28feaf361e3df6a23a48c9dc948
fcribas <rakky26@gmail.com>
2022-03-31 00:30:07 -0400

    added more detail

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,8 +4,17 @@ import Cara from "../../assets/cover/cara.jpg";
 function About() {
   return (
     <section className="container">
-      <div className="card my-2" style={{ border: "1px solid", width: "35rem", background:"#F8F8FF"}}>
-        <img src={Cara} className="card-img-top my-2" alt="Una Foto Aqui" style={{height:"500px", width: "85%"}}></img>
+      <div
+        className="card row my-2"
+        style={{ border: "1px solid", width: "35rem", background: "#F8F8FF" }}
+      >
+        <img
+          src={Cara}
+          className="card-img-top my-2"
+          alt="Una Foto Aqui"
+          style={{ height: "500px", width: "auto" }}
+        ></img>
+
         <div className="card-body">
           <h2 className="card-title"> My Portfolio </h2>
           <p className="card-text my-2">
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,22 +2,23 @@ import React from "react";
 
 function Contact() {
   return (
-    <section className="container col-6 g-3 my-2 " class="form" style={{ border: "1px solid",  }}>
+    <section className="container">
+    <div className="card row g-3 my-3 " class="form" style={{ border: "1px solid",  background: "#F8F8FF"}}>
+      <div className="my-2 col">
+        <label className="col-4 form-label my-2">First name
+          <input className="form-control my-2" style={{type:"text"}} placeholder="First name" aria-label="First name"></input></label>
 
-      <div className="my-2">
-        <label className="col-6 my-2">First name
-          <input className="form-control my-2" placeholder="First name" aria-label="First name"></input></label>
-
-        <label className="col-6 my-2">Last name
-        <input className="form-control my-2" placeholder="Last name" aria-label="Last name"></input></label>
+        <label className="col-4 form-label my-2">Last name
+        <input className="form-control my-2" style={{type:"text"}} placeholder="Last name" aria-label="Last name"></input></label>
   
   <div className="my-2">
-        <label className="col-6 form-label my-2">E-mail
-        <input className="form-control my-2" placeholder="E-mail" aria-label="E-mail"></input></label>
+        <label className="col-4 form-label my-2">E-mail
+        <input className="form-control my-2" style={{type:"text"}} placeholder="E-mail" aria-label="E-mail"></input></label>
 
-        <label className="col-6 form-label my-2">Telephone
-        <input className="form-control my-2" placeholder="Telephone" aria-label="Telephone"></input></label>
+        <label className="col-4 form-label my-2">Telephone
+        <input className="form-control my-2" style={{type:"text"}} placeholder="Telephone" aria-label="Telephone"></input></label>
         </div>
+      </div>
       </div>
     </section>
   );
