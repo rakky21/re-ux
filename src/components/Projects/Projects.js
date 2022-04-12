a229ecc2832fd064a4699d10de5e08cdf7010e8c
rakky21 <rakky26@gmail.com>
2022-04-11 20:25:47 -0400

    updated the projcets

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,15 @@
 import React from "react";
+import Toolkit from "../../assets/projects/toolkit.jpg";
 
+// Needs to create a whole new model with each project for?
 function Projects() {
+  function openFile(e) {
+    e.preventDefault();
+    console.log("clicked");
+  }
+
+  const pictures = [];
+
   return (
     <section className="my-2">
       <div className="modal-dialog">
@@ -9,11 +18,26 @@ function Projects() {
             <h2 className="modal-title"> Title</h2>
             <button className="btn-close"></button>
           </div>
-          <div className="modal-body">
-            <p> Body </p>
+          <div className="modal-body" target="_blank" rel="noreferrer noopener">
+            <div>
+              <img
+                className=""
+                src={Toolkit}
+                alt="This is the foto"
+                style={{ height: "500px", width: "auto" }}
+              ></img>
+            </div>
           </div>
           <div className="modal-footer">
-            <button className="button btn btn-primary">Open</button>
+            <a
+              onClick={{ openFile }}
+              className="button btn btn-primary"
+              href="https://lrivales.github.io/app-toolkit/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Open
+            </a>
           </div>
         </div>
       </div>
@@ -22,39 +46,3 @@ function Projects() {
 }
 
 export default Projects;
-
-
-
-
-// import React, { useEffect } from "react";
-// import { capitalizeFirstLetter } from "../../utils/helpers";
-
-// function Projects(props) {
-
-//   const { projectos = [], setCurrentProject, currentProject } = props;
-
-//   useEffect (() => {
-//     document.title = capitalizeFirstLetter(currentProject);
-//   }, [currentProject]);
-
-//   return (
-//     <section className="my-2">
-//       <div className="modal-dialog">
-//         <div className="modal-content " style={{ background: "#F8F8FF" }}>
-//           <div className="modal-header">
-//             <h2 className="modal-title"> Title</h2>
-//             <button className="btn-close"></button>
-//           </div>
-//           <div className="modal-body">
-//             <p> Body </p>
-//           </div>
-//           <div className="modal-footer">
-//             <button className="button btn btn-primary">Open</button>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default Projects;
