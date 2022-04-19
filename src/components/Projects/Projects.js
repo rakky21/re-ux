15dd489e59d6c7922dcf0e13024603df0e04451b
fcribas <rakky26@gmail.com>
2022-04-19 00:30:07 -0400

    added additional cards to display other projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 // import React from "react";
 import Toolkit from "../../assets/projects/toolkit.jpg";
+import Tunebook from "../../assets/projects/tunebook.jpg";
 
 // Needs to create a whole new model with each project for?
 function Projects() {
@@ -13,37 +14,103 @@ function Projects() {
     {
       name: "Toolkit",
       href: "https://lrivales.github.io/app-toolkit/",
-      description: "This is the first group project"
+      description: "This is the first group project",
     },
     {
       name: "2",
       href: "/",
-      description: "This is the second group project"
+      description: "This is the second group project",
     },
     {
       name: "3",
       href: "/",
-      description: "This is the third group project"
-    }
+      description: "This is the third group project",
+    },
   ]);
 
   return (
     <section className="container">
-    <div className="my-2 card" style={{ width: "18rem"}}>
-      <img
-        className="card-img-top"
-        src={Toolkit}
-        alt= {cards.name}
-      ></img>
-      <a
-        onClick={{ openFile }}
-        className="button btn btn-primary"
-        href="https://lrivales.github.io/app-toolkit/"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        Open
-      </a>
+      <div className="row">
+        <div className="my-2 card" style={{ width: "18rem" }}>
+          <img className="card-img-top" src={Toolkit} alt={cards.name}></img>
+          <a
+            onClick={{ openFile }}
+            className="button btn btn-primary col-4"
+            href="https://lrivales.github.io/app-toolkit/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Open
+          </a>
+        </div>
+
+        <div className="my-2 card" style={{ width: "18rem" }}>
+          <img className="card-img-top" src={Tunebook} alt={cards.name}></img>
+          <a
+            onClick={{ openFile }}
+            className="button btn btn-primary col-4"
+            href="https://lrivales.github.io/app-toolkit/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Open
+          </a>
+        </div>
+
+        <div className="my-2 card" style={{ width: "18rem" }}>
+          <img className="card-img-top" src={Tunebook} alt={cards.name}></img>
+          <a
+            onClick={{ openFile }}
+            className="button btn btn-primary col-4"
+            href="https://lrivales.github.io/app-toolkit/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Open
+          </a>
+        </div>
+
+      </div>
+      <div className="row">
+        <div className="my-2 card" style={{ width: "18rem" }}>
+          <img className="card-img-top" src={Toolkit} alt={cards.name}></img>
+          <a
+            onClick={{ openFile }}
+            className="button btn btn-primary col-4"
+            href="https://lrivales.github.io/app-toolkit/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Open
+          </a>
+        </div>
+
+        <div className="my-2 card" style={{ width: "18rem" }}>
+          <img className="card-img-top" src={Tunebook} alt={cards.name}></img>
+          <a
+            onClick={{ openFile }}
+            className="button btn btn-primary col-4"
+            href="https://lrivales.github.io/app-toolkit/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Open
+          </a>
+        </div>
+        
+        <div className="my-2 card" style={{ width: "18rem" }}>
+          <img className="card-img-top" src={Tunebook} alt={cards.name}></img>
+          <a
+            onClick={{ openFile }}
+            className="button btn btn-primary col-4"
+            href="https://lrivales.github.io/app-toolkit/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Open
+          </a>
+        </div>
+
       </div>
     </section>
   );
