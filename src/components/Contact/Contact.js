d59d298d41debd5322ba8a7296500c2a7785a975
rakky21 <rakky26@gmail.com>
2022-04-18 21:42:24 -0400

    changed the layout of the contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,35 +2,31 @@ import React from "react";
 
 function Contact() {
   return (
-    <section className="container center" >
+    <section className="container" >
       <div className="row" style={{ background: "#F8F8FF" }} >
       <h1 className="my-3"> Freddy Rakky21 </h1>
       <p> Submit to request resume </p>
-      <div className="mb-3 col-4">
-        <label className="form-label">Email adddress</label>
-        <input className="form-control col-6" placeholder="email@example.com" type="email"></input>
-      </div>
 
-      <div className="mb-3 col-4">
+      <div className="mb-3 row">
+        <div className="col-4"> 
         <label className="form-label">Email adddress</label>
         <input className="form-control" placeholder="email@example.com" type="email"></input>
-      </div>
+      </div></div>
 
-      <div className="mb-3 col-4">
+      <div className="mb-3 row">
+        <div className="col-4">
         <label className="form-label">Email adddress</label>
         <input className="form-control" placeholder="email@example.com" type="email"></input>
-      </div>
+      </div></div>
 
-      {/* <div className="mb-3 col-4">
-        <label className="form-label">Email adddress</label>
-        <input className="form-control" placeholder="email@example.com" type="email"></input>
-      </div> */}
-
-      <div className="mb-3 col-4">
+      <div className="mb-3 row">
+        <div className="col-4">
         <label className="form-label">Comment</label>
         <textarea className="form-control" rows="3"></textarea>
       </div>
       </div>
+      </div>
+  
     </section>
   );
 }
