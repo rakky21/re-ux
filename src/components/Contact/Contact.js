5816b466d008a74c20e165e8b0d0710331348bcf
fcribas <rakky26@gmail.com>
2022-06-23 12:40:07 -0400

    layout and styling of contact complete

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// setState?
 
 export default function Contact() {
   const [name, setName] = useState("");
@@ -7,47 +6,35 @@ export default function Contact() {
   const [correo, setCorreo] = useState("");
 
   return (
-    <section className="container">
-      <div className="contact">
-        <h1> Contact</h1>
-        <div>
-          <label> First Name:</label>
-          <input
-            placeholder="John"
-            type="text"
-            name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label> Last Name:</label>
-          <input
-            placeholder="Smith"
-            type="text"
-            value={apellido}
-            onChange={(e) => setApellido(e.target.value)}
-          />
-        </div>
-        <div>
-          <label> Email adddress:</label>
-          <input
-            placeholder="email@example.com"
-            type="email"
-            value={correo}
-            onChange={(e) => setCorreo(e.target.value)}
-          />
-        </div>
-        <div>
-          <label> Comment:</label>
-          <textarea required placeholder="Comment" rows="3"></textarea>
-        </div>
+    <form className="contact">
+      <h1> Contact </h1>
+      <div>
+        <label> First Name:</label>
         <input
-          type="submit"
-          value="submit"
-          className="button btn btn-primary col-4"
+          placeholder="John"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
+        <label> Last Name</label>
+        <input
+          placeholder="Smith"
+          type="text"
+          value={apellido}
+          onChange={(e) => setApellido(e.target.value)}
+        />
+        <label> Email </label>
+        <input
+          placeholder="email@email.com"
+          type="text"
+          value={correo}
+          onChange={(e) => setCorreo(e.target.value)}
+        />
+        <label> Comment</label>
+        <textarea placeholder="Comment"></textarea>
       </div>
-    </section>
+      <button type="Submit"> Submit</button>
+      <p> {name}</p>
+    </form>
   );
 }
