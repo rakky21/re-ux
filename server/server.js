17d257d1c7fb3d754e98f2744b1f63d948044b86
fcribas <rakky26@gmail.com>
2022-10-04 12:35:58 -0400

    updating

--- server/server.js
+++ server/server.js
@@ -6,41 +6,43 @@ const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
-const morgan = require("morgan");
-
-// EXPRESS SERVER
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// START APOLLO
-const startServer = async () => {
+const server = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware,
   });
-  await server.start();
-  server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
-startServer();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-// Serve up static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+  // Serve up static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
-});
-//  HTTP request logger
-app.use(morgan("tiny"));
+  // startServer();
+  const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    db.once("open", () => {
+      app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(
+          `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+        );
+      });
+    });
+  };
+};
+
+// //  HTTP request logger
+// const morgan = require("morgan");
+// app.use(morgan("tiny"));
+startApolloServer(typeDefs, resolvers);
