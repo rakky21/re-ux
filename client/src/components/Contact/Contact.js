2c9f981189c6a777e44b72bfb99a77b96d221d7c
Azrakky <fcribas1@gmail.com>
2022-11-07 15:42:50 -0500

    update

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import { useState } from "react";
-
-export default function Contact() {
-  const [name, setName] = useState("");
-  const [apellido, setApellido] = useState("");
-  const [correo, setCorreo] = useState("");
-  const [comentario, setComentario] = useState("");
-  const [isPending, setIsPending] = useState(false);
-
-  function getPersonas(e) {
-    e.preventDefault();
-    setIsPending(true);
-=======
 import React from "react";
 import { AiOutlineMail } from "react-icons/ai";
 
@@ -22,30 +8,14 @@ function Contact() {
       <div className="contact">
         <label> Name </label>
         <input></input>
->>>>>>> 2bc258d13c370ffb9b233e0f98b7c4d2943510ee
 
         <label> Last Name </label>
         <input></input>
 
-<<<<<<< HEAD
-    fetch("http://localhost:3001/personas", {
-      method: "POST",
-      mode: "cors",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(personas),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("new entry");
-        setIsPending(false);
-      });
-  }
-=======
         <label> Email </label>
         <input></input>
 
         <textarea></textarea>
->>>>>>> 2bc258d13c370ffb9b233e0f98b7c4d2943510ee
 
         <div>
           {" "}
