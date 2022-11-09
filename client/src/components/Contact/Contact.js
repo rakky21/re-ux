bc684dcaf00310d5ea70ffc062e2d59b9163ef8b
fcribas <rakky26@gmail.com>
2022-11-08 21:55:19 -0500

    updated the contaact section

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,28 +1,69 @@
 import React from "react";
-import { AiOutlineMail } from "react-icons/ai";
+import { useState } from "react";
+import { AiOutlineMail, AiOutlineLoading3Quarters } from "react-icons/ai";
 
 function Contact() {
+  const [name, setName] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [comentario, setComentario] = useState("");
+  const [isPending, setIsPending] = useState(false);
+
+  const handleRequisito = (e) => {
+    e.preventDefault();
+    setIsPending(true);
+  };
   return (
     <section id="contact">
-      <h2> CONTACT ME </h2>
+      <h2> Contact </h2>
       <div className="contact">
-        <label> Name </label>
-        <input></input>
+        <form className="form" onSubmit={handleRequisito}>
+          <label> First Name:</label>
+          <input
+            required
+            placeholder="John"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
 
-        <label> Last Name </label>
-        <input></input>
-
-        <label> Email </label>
-        <input></input>
-
-        <textarea></textarea>
-
-        <div>
-          {" "}
-          <a className="btn" href="email">
-            <AiOutlineMail size="2rem" /> Contact Me{" "}
-          </a>
-        </div>
+          <label> Last Name: </label>
+          <input
+            required
+            placeholder="Smith"
+            type="text"
+            value={apellido}
+            onChange={(e) => setApellido(e.target.value)}
+          />
+          <label> Email </label>
+          <input
+            required
+            placeholder="email@email.com"
+            type="text"
+            value={correo}
+            onChange={(e) => setCorreo(e.target.value)}
+          />
+          <label> Comment</label>
+          <textarea
+            placeholder="Comment"
+            typeof="text"
+            value={comentario}
+            onChange={(e) => setComentario(e.target.value)}
+          />
+          <div>
+            {!isPending && (
+              <button className="btn" typeof="submit">
+                {" "}
+                <AiOutlineMail /> Contact Me
+              </button>
+            )}
+            {isPending && (
+              <div disabled className="btn">
+                <AiOutlineLoading3Quarters /> Loading{" "}
+              </div>
+            )}
+          </div>
+        </form>
       </div>
     </section>
   );
